628b9787c21b37ca47454efd8313053132abfd2e
Raku <rakusama80@gmail.com>
2025-08-07 10:37:30 +0800

    quiz2 is done

---
+++ quiz/part1/quiz2.js
@@ -0,0 +1,24 @@
+let input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"];
+
+function dataHandling(input) {
+    input.splice(1,1, "Roman Alamsyah Elsharawy");
+    input.splice(2,1, "Provinsi Bandar Lampung");
+    input.splice(4,4, "Pria", "SMA International Metro");
+    console.log(input);
+    input.splice(5,5, "Mei");
+    console.log(input[5])
+    let reverseElement = input[3].split("/");
+    console.log(reverseElement.reverse());
+    let dashedElement = input[3].split("/");
+    console.log(dashedElement.join("-"));
+    let limitChar = input[1];
+    limitChar = limitChar.slice(0, 15);
+    console.log(limitChar);
+}                           
+
+dataHandling(input);
+
+//function menerima input dari variabel input
+//input menggunakan splice method
+//mencetak hasil input
+//input menggunakan splice
