7b86f3e2716d3b201d9e3de3a52731fdbc16da52
Raku <rakusama80@gmail.com>
2025-08-07 20:49:50 +0800

    update quiz2 untuk output mei

--- quiz/part1/quiz2.js
+++ quiz/part1/quiz2.js
@@ -5,12 +5,19 @@ function dataHandling(input) {
     input.splice(2,1, "Provinsi Bandar Lampung");
     input.splice(4,4, "Pria", "SMA International Metro");
     console.log(input);
-    input.splice(5,5, "Mei");
-    console.log(input[5])
+    //get a month
+    let dateArr = input[3];
+    let splitDate = dateArr.split('/');
+    let reverseDate = splitDate.reverse();
+    const date = new Date(reverseDate);
+    const month = date.toLocaleString('id-ID', {month:'long'})
+    console.log(month);
+    //join -
     let reverseElement = input[3].split("/");
     console.log(reverseElement.reverse());
     let dashedElement = input[3].split("/");
     console.log(dashedElement.join("-"));
+    //limit the character
     let limitChar = input[1];
     limitChar = limitChar.slice(0, 15);
     console.log(limitChar);
